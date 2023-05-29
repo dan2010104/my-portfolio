1fe00bb73198f4272e496b65f92801d71f4b4991
Daniel Wyson <dwyson0@gmail.com>
2023-05-29 22:17:04 +0200

    validation in Javascript

--- myScript.js
+++ myScript.js
@@ -24,6 +24,41 @@ function changeimg(){
 	setTimeout("changeimg()", time);
 }
 
-window.onload = changeimg;
 
+//greetings
+
+function getGreeting() {
+	var date = new Date();
+	var hours = date.getHours();
+	var greeting;
+
+	if (hours >= 0 && hours < 12) {
+		greeting =" Goodmorning!, How are you?";
+	}
+	else if (hours >= 12 && hours < 18) {
+		greeting = " Good Afternoon!, How are you?";
+	}
+	else {
+		greeting = " Good Evening!, How are you?";
+	}
+	return greeting;
+}
+
+function displayGreeting(){
+	var greeting = getGreeting();
+	document.getElementById("greeting").textContent = greeting;
+}
+
+//form validation
+
+function validateform(){
+	var firstname = document.myform.firstname.value;
+	var surname = document.myform.surname.value;
+
+	if (firstname=="null" || firstname=="") {
+		alert("firstname can't be empty!");
+		return false;
+	}
+
+}
 
